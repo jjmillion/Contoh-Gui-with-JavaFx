b6f616a2cf116d2227b17e3744a2eedcd487f827
Test <test@example.com>
2018-02-13 21:10:36 +0700

    February 13 - JavaFx with GUI update radio button jenis kelamin (Imperfect) - 1.01

--- src/ContohGuiController.java
+++ src/ContohGuiController.java
@@ -1,3 +1,4 @@
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -6,9 +7,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.KeyEvent;
 
 public class ContohGuiController implements Initializable {
+
     @FXML
     TextField txtNama;
     @FXML
@@ -19,28 +23,48 @@ public class ContohGuiController implements Initializable {
     Label lblSeen;
     @FXML
     Label lblNotification;
-    
-    
-    
-    
+    @FXML
+    RadioButton pria;
+    @FXML
+    RadioButton wanita;
+    @FXML
+    ToggleGroup jenisKelamin;
+
+    private String title;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        txtNama.setOnKeyTyped(this::onTxtNamaType);
+        txtNama.setOnKeyReleased(this::onTxtNamaTyping);
+        pria.setOnAction(this::onJenisKelaminChanged);
+        wanita.setOnAction(this::onJenisKelaminChanged);
+        
+    
     }
 
     @FXML
     public void onBtnHelloClick(ActionEvent event) {
-        lblHello.setText("Hello " + txtNama.getText());
+        lblHello.setText("Hello " + this.title + txtNama.getText());
         txtNama.setText("");
         lblNotification.setText("Good Day Mate :)");
         lblSeen.setText("");
     }
-    
+
     @FXML
-    public void onTxtNamaType(KeyEvent event) {
+    public void onTxtNamaTyping(KeyEvent event) {
+        //This title di label tidak auto change
         lblHello.setText("");
-        lblSeen.setText("Someone is Typing . . .");
+        lblSeen.setText("Hello "+ this.title + " " + txtNama.getText());
         lblNotification.setText("");
     }
-    
+
+    @FXML
+    public void onJenisKelaminChanged(ActionEvent event) {
+        if (jenisKelamin.getSelectedToggle().equals(pria)) {
+            this.title = "Mr. ";
+        } else if (jenisKelamin.getSelectedToggle().equals(wanita)) {
+            this.title = "Mrs. ";
+        }
+
+    }
+
 }
